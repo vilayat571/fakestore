7873fa71689ee4474a47aeb14cbed9b0bae4b62e
vilayat571 <vilayat571@.com>
2023-04-14 15:57:23 -0600

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./pages/Main/Main";
-import Navbar from "./components/Navbar/Navbar";
-import Results from "./pages/Results/Results";
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/results" element={<Results />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <div>
+      Clearly page
+    </div>
+  )
 }
-export default App;
--- src/layout/Layout.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-
-interface IChildren {
-  children: ReactNode;
-}
-export default function Layout(props: IChildren) {
-  return <>{props.children}</>;
-}
